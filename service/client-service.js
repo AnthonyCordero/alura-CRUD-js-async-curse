4584305c3b3b9e07c8a647f0a5166d6c3896a8ea
Anthony Cordero <tony170593@gmail.com>
2023-08-01 19:44:13 -0600

    Clase 01 - Terminada

--- service/client-service.js
+++ service/client-service.js
@@ -1,4 +1,28 @@
-const http = new XMLHttpRequest();
+const crearNuevaLinea = (nombre, email) => {
+    const linea = document.createElement('tr');
+    const contenido = `
+        <td class="td" data-td>
+        ${nombre}
+        </td>
+        <td>${email}</td>
+        <td>
+        <ul class="table__button-control">
+            <li>
+            <a href="../screens/editar_cliente.html" class="simple-button simple-button--edit">Editar</a>
+            </li>
+            <li>
+            <button class="simple-button simple-button--delete" type="button">
+                Eliminar
+            </button>
+            </li>
+        </ul>
+        </td>`
+    linea.innerHTML = contenido;
+    return linea;
+};
+const table = document.querySelector('[data-table]')
+
+
 
 
 // Abre http (metodo,url)
@@ -9,12 +33,30 @@ const http = new XMLHttpRequest();
 // Uupdate  - PUT/PATCH
 // Ddelete  - DELETE
 
+const listaClientes = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+        http.open('GET', 'http://localhost:3000/perfil');
 
-http.open('GET', 'http://localhost:3000/perfil');
+        http.send();
 
-http.send();
-http.onload = () => {
-    const data = http.response;
-    console.log(data);
-    console.log(data);
-}
+        http.onload = () => {
+            const response = JSON.parse(http.response);
+            if (http.status >= 400) {
+                reject(response);
+            } else {
+                resolve(response);
+            }
+        };
+    });
+    return promise;
+};
+
+listaClientes().then((data) => {
+    data.forEach((perfil) => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+        table.appendChild(nuevaLinea);
+    });
+})
+    .catch((error) => alert('Ocurrió un error'));
+

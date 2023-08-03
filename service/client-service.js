a9d1294fbe3eeb6ccbf48b2a051765ad26a1aabd
Anthony Cordero <tony170593@gmail.com>
2023-08-02 19:34:46 -0600

    clase02 Finished

--- service/client-service.js
+++ service/client-service.js
@@ -1,30 +1,3 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement('tr');
-    const contenido = `
-        <td class="td" data-td>
-        ${nombre}
-        </td>
-        <td>${email}</td>
-        <td>
-        <ul class="table__button-control">
-            <li>
-            <a href="../screens/editar_cliente.html" class="simple-button simple-button--edit">Editar</a>
-            </li>
-            <li>
-            <button class="simple-button simple-button--delete" type="button">
-                Eliminar
-            </button>
-            </li>
-        </ul>
-        </td>`
-    linea.innerHTML = contenido;
-    return linea;
-};
-const table = document.querySelector('[data-table]')
-
-
-
-
 // Abre http (metodo,url)
 
 // CRUD     - Metodos http
@@ -33,30 +6,11 @@ const table = document.querySelector('[data-table]')
 // Uupdate  - PUT/PATCH
 // Ddelete  - DELETE
 
-const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest();
-        http.open('GET', 'http://localhost:3000/perfil');
+// Fetch API
 
-        http.send();
+const listaClientes = () => fetch('http://localhost:3000/perfil').then((respuesta) => respuesta.json());
 
-        http.onload = () => {
-            const response = JSON.parse(http.response);
-            if (http.status >= 400) {
-                reject(response);
-            } else {
-                resolve(response);
-            }
-        };
-    });
-    return promise;
-};
 
-listaClientes().then((data) => {
-    data.forEach((perfil) => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-})
-    .catch((error) => alert('Ocurrió un error'));
-
+export const clientServices = {
+    listaClientes,
+}
